f84c65c0274805168be18a11a8027a10ecb3e153
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-07-26 21:04:20 +0300

    add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
     <div className={styles.footer}>
       <div className={styles.container}>
         <img className={styles.logo} src={Logo_footer} alt='/' />
-        <p>
-          <span className={styles.footer_text}>© 2022 All Right Reserved</span>
-        </p>
+
+        <span>© 2022 All Right Reserved</span>
+
         <button>Share a Car</button>
       </div>
     </div>
